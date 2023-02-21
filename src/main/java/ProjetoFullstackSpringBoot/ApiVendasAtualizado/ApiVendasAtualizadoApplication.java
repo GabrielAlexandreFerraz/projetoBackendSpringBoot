4f6c8186a942b4ed7a9984db4f38b806b29a4fb7
GabrielAlexandreFerraz <gblferrazrodrigues@gmail.com>
2023-02-21 20:07:41 -0300

    comecando a api

---
+++ src/main/java/ProjetoFullstackSpringBoot/ApiVendasAtualizado/ApiVendasAtualizadoApplication.java
@@ -0,0 +1,13 @@
+package ProjetoFullstackSpringBoot.ApiVendasAtualizado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVendasAtualizadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVendasAtualizadoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ProjetoFullstackSpringBoot/ApiVendasAtualizado/repository/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package ProjetoFullstackSpringBoot.ApiVendasAtualizado.repository;
+
+import ProjetoFullstackSpringBoot.ApiVendasAtualizado.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+}
