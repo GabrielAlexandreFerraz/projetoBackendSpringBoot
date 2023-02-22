e7c0feb1ec71ae6ea4dc3dd094c6490d7cf42770
GabrielAlexandreFerraz <gblferrazrodrigues@gmail.com>
2023-02-22 17:52:19 -0300

    metodos put e delete

--- src/main/java/ProjetoFullstackSpringBoot/ApiVendasAtualizado/restProdutos/ProdutoController.java
+++ src/main/java/ProjetoFullstackSpringBoot/ApiVendasAtualizado/restProdutos/ProdutoController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -16,6 +19,21 @@ public class ProdutoController {
     @Autowired
     private ProdutoRepository repository;
 
+    @GetMapping
+    public List<ProdutoFormRequest> getLista(){
+        return repository.findAll().stream()
+                .map(produt -> ProdutoFormRequest.fromModel(produt) ).collect(Collectors.toList());
+    }
+
+    @GetMapping("{id}")
+    public  ResponseEntity<ProdutoFormRequest> getById(@PathVariable Long id){
+        Optional<Produto> produtoExistente = repository.findById(id);
+        if(produtoExistente.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        ProdutoFormRequest produto = produtoExistente.map(ProdutoFormRequest::fromModel).get();
+        return ResponseEntity.ok(produto);
+    }
 
     @PostMapping
     public ProdutoFormRequest salvar(@RequestBody ProdutoFormRequest produto){
@@ -34,6 +52,16 @@ public class ProdutoController {
         entidade.setId(id);
         repository.save(entidade);
         return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id, @RequestBody ProdutoFormRequest produto){
+        Optional<Produto> produtoExistente = repository.findById(id);
+        if(produtoExistente.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        repository.delete(produtoExistente.get());
+        return ResponseEntity.noContent().build();
     }
 
 }
